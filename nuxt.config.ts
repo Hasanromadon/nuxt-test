a88c04c178e0422383bfd1439830976f1f17af89
hasan <hsanromadon@gmail.com>
2022-08-05 16:39:47 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+import { defineNuxtConfig } from 'nuxt';
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  plugins: ['~/plugins/kontenbase.js'],
+});
---
+++ plugins/kontenbase.js
@@ -0,0 +1,7 @@
+import { KontenbaseClient } from '@kontenbase/sdk';
+
+export default function kontenbase() {
+  new KontenbaseClient({
+    apiKey: '0c3a64ad-957c-4d9f-bf8d-75222d4ca2ad',
+  });
+}
